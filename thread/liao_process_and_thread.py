22b70792ef0718a8a3c6752c9947abcac54badce
zhoujianchao <zhoujianchao@ppdai.com>
2018-10-09 23:47:16 +0800

    add some threadlocal etc ;

--- thread/liao_process_and_thread.py
+++ thread/liao_process_and_thread.py
@@ -90,5 +90,51 @@ def func5():
     pr.terminate()
 
 
+def func6():
+    import time, threading
+
+    def loop():
+        print('thread %s is running...' % threading.current_thread().name)
+        n = 0
+        while n < 5:
+            n = n + 1
+            print('thread %s >>> %s' % (threading.current_thread().name, n))
+            time.sleep(1)
+        print('thread %s is running...' % threading.current_thread().name)
+
+    print('thread %s is running...' % threading.current_thread().name)
+    t = threading.Thread(target=loop, name='LoopThread')
+    t.start()
+    t.join()
+    print('thread %s ended.' % threading.current_thread().name)
+
+
+def func7():
+    import time, threading
+    balance = 0
+    lock = threading.Lock()
+
+    def change_it(n):
+        nonlocal balance
+        balance = balance + n
+        balance = balance - n
+
+    def run_thread(n):
+        for i in range(100000):
+            lock.acquire()
+            try:
+                change_it(n)
+            finally:
+                lock.release()
+
+    t1 = threading.Thread(target=run_thread, args=(5,))
+    t2 = threading.Thread(target=run_thread, args=(8,))
+    t1.start()
+    t2.start()
+    t1.join()
+    t2.join()
+    print(balance)
+
+
 if __name__ == '__main__':
-    func3()
+    func7()
